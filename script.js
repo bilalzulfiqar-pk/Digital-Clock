667eef62c8ca032c1f08b6c3979d35c285599ab5
Muhammad Bilal Zulfiqar <bilalzulfiqar34@gmail.com>
2025-02-06 11:14:55 +0500

    first

---
+++ script.js
@@ -0,0 +1,41 @@
+const days = [ 'sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat'];
+const months = ['JAN' , 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'];
+
+const $ = (id) => document.getElementById(id);
+const zeroPad = (num) => num.toString().padStart(2, '0');
+
+function updateClock() {
+    const now = new Date();
+    const hours = zeroPad(now.getHours());
+    const minutes = zeroPad(now.getMinutes());
+    const seconds = zeroPad(now.getSeconds()); 
+    const day = days[now.getDay()];
+    const month = months[now.getMonth()];
+    const date = zeroPad(now.getDate());
+    const year = now.getFullYear();
+
+
+    // console.log(`${hours}:${minutes}:${seconds} ${day} ${month} ${date} ${year}`);
+
+    // console.log(now);
+
+    if (hours >= 12) {
+        $('ampm').innerHTML = 'PM';
+        hours = zeroPad(hours - 12);
+    } else {
+        $('ampm').innerHTML = 'AM';
+    }
+
+    $('hour').innerHTML = hours;
+    $('min').innerHTML = minutes;
+    $('sec').innerHTML = seconds;
+    $('day').innerHTML = date;
+    $('month').innerHTML = month;
+    $('year').innerHTML = year;
+
+    $(`${day}`).classList.add('active');
+
+    // $('hour').style.transform = `rotate(${hours * 30}deg)`;
+}
+
+setInterval(updateClock, 1000);
